8ebdc47bea535034a23d20005c703ee8c03b11a4
LautaroDiana <lautarodiana@gmail.com>
2025-01-25 19:42:57 -0300

    ex 4.1 done

--- bloglist/app.js
+++ bloglist/app.js
@@ -3,28 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const BlogRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', BlogRouter)
 
 module.exports = app
