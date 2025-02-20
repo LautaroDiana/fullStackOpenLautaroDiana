91ea5c08509f370f6b2ddbc7a17f91eed33059c8
LautaroDiana <lautarodiana@gmail.com>
2025-02-20 18:16:36 -0300

    Part 7 started

---
+++ part7/vite-project/src/main.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+// import Counter from './Counter'
+// import Form from './Form'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+        <App />
+        {/* <Counter /> */}
+        {/* <Form /> */}
+    </Router>
+)
